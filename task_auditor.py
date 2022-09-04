26147aed6400b129d1e5263055aebc2993a92b66
Kate Wang <kate99w@gmail.com>
2022-09-04 13:17:39 -0700

    fix timezone bugs

--- task_auditor.py
+++ task_auditor.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from datetime import datetime
+from pytz import timezone
 
 import click
 
@@ -18,8 +20,11 @@ def main(notion_token: str, notion_db_id: str, day: int, show_details: bool):
     for log in logs:
         point = log.process_raw_log().point
         count[log.owner] += point
+        log_date = datetime.strptime(
+            log.created_at_str, "%Y-%m-%dT%H:%M:%S.%f%z"
+        ).astimezone(timezone("America/Los_Angeles"))
         details[log.owner].append(
-            f"\t{point} point(s) earned by `{log.raw_log}` at {log.created_at_str[5:16].replace('T', ' ')}"
+            f"\t{point} point(s) earned by `{log.raw_log}` at {log_date.strftime('%m-%d %H:%M')}"
         )
 
     for k, v in count.items():
